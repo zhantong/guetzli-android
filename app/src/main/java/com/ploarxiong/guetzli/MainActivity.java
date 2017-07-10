0bd1082604b0606804648f2a131c9879ae2c2a92
zhantong <zhantong1994@163.com>
2017-07-10 21:55:52 +0800

    实现调用guetzli压缩图片

--- app/src/main/java/com/ploarxiong/guetzli/MainActivity.java
+++ app/src/main/java/com/ploarxiong/guetzli/MainActivity.java
@@ -1,9 +1,14 @@
 package com.ploarxiong.guetzli;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_ORIGIN_IMAGE_PATH = 1;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -14,11 +19,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnStart = (Button) findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start();
+            }
+        });
+    }
+
+    void start() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(intent, REQUEST_CODE_ORIGIN_IMAGE_PATH);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_CODE_ORIGIN_IMAGE_PATH) {
+                Uri uri = data.getData();
+                System.out.println("started");
+                compressImage(uri.getPath(), uri.getPath() + "1");
+                System.out.println("finished");
+            }
+        }
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native int compressImage(String inputImagePath,String outputImagePath);
+    public native int compressImage(String inputImagePath, String outputImagePath);
 }
