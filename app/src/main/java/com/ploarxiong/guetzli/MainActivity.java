a0e825df161efdb24b4a5417564d0468e540568e
zhantong <zhantong1994@163.com>
2017-07-21 19:55:10 +0800

    优化调试UI界面

--- app/src/main/java/com/ploarxiong/guetzli/MainActivity.java
+++ app/src/main/java/com/ploarxiong/guetzli/MainActivity.java
@@ -1,13 +1,16 @@
 package com.ploarxiong.guetzli;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    String imagePath;
     public static final int REQUEST_CODE_ORIGIN_IMAGE_PATH = 1;
 
     // Used to load the 'native-lib' library on application startup.
@@ -20,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnChooseImage = (Button) findViewById(R.id.btn_choose_image);
+        btnChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+
         Button btnStart = (Button) findViewById(R.id.btn_start);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,11 +40,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    void start() {
+    void chooseImage() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("*/*");
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         startActivityForResult(intent, REQUEST_CODE_ORIGIN_IMAGE_PATH);
+    }
+
+    void start() {
+        if (imagePath == null) {
+            imagePath = "/storage/emulated/0/bees.png";
+        }
+        System.out.println("start");
+        compressImage(imagePath, imagePath + "1");
+        System.out.println("finish");
     }
 
     @Override
@@ -41,9 +61,9 @@ public class MainActivity extends AppCompatActivity {
         if (resultCode == RESULT_OK) {
             if (requestCode == REQUEST_CODE_ORIGIN_IMAGE_PATH) {
                 Uri uri = data.getData();
-                System.out.println("started");
-                compressImage(uri.getPath(), uri.getPath() + "1");
-                System.out.println("finished");
+                TextView textImagePath = (TextView) findViewById(R.id.text_image_path);
+                imagePath = contentUriToFileUri(uri);
+                textImagePath.setText(imagePath);
             }
         }
     }
@@ -53,4 +73,17 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native int compressImage(String inputImagePath, String outputImagePath);
+
+    String contentUriToFileUri(Uri _uri) {
+        String filePath;
+        if (_uri != null && "content".equals(_uri.getScheme())) {
+            Cursor cursor = getContentResolver().query(_uri, new String[]{android.provider.MediaStore.Images.ImageColumns.DATA}, null, null, null);
+            cursor.moveToFirst();
+            filePath = cursor.getString(0);
+            cursor.close();
+        } else {
+            filePath = _uri.getPath();
+        }
+        return filePath;
+    }
 }
